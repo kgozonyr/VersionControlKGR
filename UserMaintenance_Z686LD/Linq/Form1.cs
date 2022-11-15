2c3653f1ddebd376cef9ccefd71570ee58beb3e6
Rita Ildikó Kókainé Gózony <kgozonyr@gmail.com>
2022-11-15 22:21:07 +0100

    2. feladat orszagok beolvasasa

--- UserMaintenance_Z686LD/Linq/Form1.cs
+++ UserMaintenance_Z686LD/Linq/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,38 @@ namespace Linq
 {
     public partial class Form1 : Form
     {
+        private List<Country> countries = new List<Country>();
+
         public Form1()
         {
             InitializeComponent();
+            LoadData("ramen.csv");
+        }
+
+        private void LoadData(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    var sor = sr.ReadLine().Split(';');
+
+                    var countryName = sor[2];
+                    var eredmeny = (from c in countries
+                                          where c.Name.Equals(countryName)
+                                          select c).FirstOrDefault();
+                    if (eredmeny == null)
+                    {
+                        eredmeny = new Country()
+                        {
+                            ID = countries.Count,
+                            Name = countryName
+                        };
+                        countries.Add(eredmeny);
+                    }
+                }
+            }
         }
     }
 }
