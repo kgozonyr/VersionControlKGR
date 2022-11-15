b52ffbf826bd19cb11a5e0cb8292bb21729a0a3c
Rita Ildikó Kókainé Gózony <kgozonyr@gmail.com>
2022-11-15 22:42:07 +0100

    3. feladat kiszervezes, ramenek hozzaadasa

--- UserMaintenance_Z686LD/Linq/Form1.cs
+++ UserMaintenance_Z686LD/Linq/Form1.cs
@@ -14,6 +14,7 @@ namespace Linq
     public partial class Form1 : Form
     {
         private List<Country> countries = new List<Country>();
+        private List<Ramen> ramens = new List<Ramen>();
 
         public Form1()
         {
@@ -31,20 +32,37 @@ namespace Linq
                     var sor = sr.ReadLine().Split(';');
 
                     var countryName = sor[2];
-                    var eredmeny = (from c in countries
-                                          where c.Name.Equals(countryName)
-                                          select c).FirstOrDefault();
-                    if (eredmeny == null)
+                    var aktorszag = AddCountry(countryName);
+                    Ramen r = new Ramen
                     {
-                        eredmeny = new Country()
-                        {
-                            ID = countries.Count,
-                            Name = countryName
-                        };
-                        countries.Add(eredmeny);
-                    }
+                        ID = ramens.Count,
+                        Brand = sor[0],
+                        Name = sor[1],
+                        CountryFK = aktorszag.ID,
+                        Country = aktorszag,
+                        Rating = Convert.ToDouble(sor[3])
+                    };
+                    ramens.Add(r);
                 }
+                
             }
+        }
+
+        private Country AddCountry(string countryName)
+        {
+            var eredmeny = (from c in countries
+                            where c.Name.Equals(countryName)
+                            select c).FirstOrDefault();
+            if (eredmeny == null)
+            {
+                eredmeny = new Country()
+                {
+                    ID = countries.Count,
+                    Name = countryName
+                };
+                countries.Add(eredmeny);                
+            }
+            return eredmeny;
         }
     }
 }
