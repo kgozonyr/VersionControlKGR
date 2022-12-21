3ed0726a458ac6c9b29d557f51e511f6bd0d4a80
Rita Ildikó Kókainé Gózony <kgozonyr@gmail.com>
2022-12-21 19:48:34 +0100

    4. Adatok betöltése

--- UserMaintenance_Z686LD/VAR/Form1.cs
+++ UserMaintenance_Z686LD/VAR/Form1.cs
@@ -12,9 +12,15 @@ namespace VAR
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        PortfolioEntities context = new PortfolioEntities();
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Tick.ToList();
+            dataGridView1.DataSource = Ticks;
+
         }
     }
 }
